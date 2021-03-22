3bda499d65466365f04461779803d2dcd7890f8a
Luca <luca.a41a@gmail.com>
2021-03-22 14:49:06 +0100

    reqVerification

--- src/app/analysis-detail/analysis-detail.component.ts
+++ src/app/analysis-detail/analysis-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AnalysisService} from '../analysis.service';
+import { ReqService } from '../req.service';
 
 @Component({
   selector: 'app-analysis-detail',
@@ -8,7 +9,9 @@ import {AnalysisService} from '../analysis.service';
 })
 export class AnalysisDetailComponent implements OnInit {
 
-  constructor(public analysisService: AnalysisService) { }
+  t:number = 0;
+
+  constructor(public analysisService: AnalysisService, public reqService: ReqService) { }
   ngOnInit(): void {
   }
 
@@ -22,4 +25,29 @@ export class AnalysisDetailComponent implements OnInit {
     }
   }
 
+  verify(row){
+    this.t++;
+    var variableString = "";
+    for (var i = 0; i< row.length; i++){
+      if(row[i]==""){
+        variableString = variableString + "var " +this.analysisService.header[i] +"="+ "null" + ";";
+      } else{
+        variableString = variableString + "var "+ this.analysisService.header[i] +"="+ row[i] + ";";
+      }     
+    }
+    var feasible = true;
+
+    for ( var i = 0; i < this.reqService.requirements.length; i++){
+      if (this.reqService.requirements[i].Selected){
+        for (var j in this.reqService.requirements[i].Constraints){
+          //    console.log(variableString + this.reqService.requirements[i].Constraints[j].Value);
+
+          // controllo eval
+          feasible = feasible && eval(variableString + this.reqService.requirements[i].Constraints[j].Value+";");
+        }
+      }
+    }
+
+    return !feasible;
+  }
 }
